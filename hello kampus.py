c0e14d62cc426c9b75051099e930835f23bb0f15
alfandiseptiawan <82552069+alfandiseptiawan@users.noreply.github.com>
2021-04-15 17:48:28 +0700

    Add files via upload

---
+++ hello kampus.py
@@ -0,0 +1,7 @@
+print("===============================")
+print("Biodataku")
+print("===============================")
+
+print("Nama Lengkap : Alfandi Septiawan")
+print("Kelas : A")
+print("Kampus: Universitas Narotama Surabaya ")
